f5bfa9c64818f1145a02a021f2bc6fe5e09f39cf
Dmitry Zaytsev <dmitry.zaytsev83@gmail.com>
2018-11-30 17:15:38 +0300

    Add very base structure

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
-echo "Hello, Internet Shop!";
+function printIndexPage() {
+    echo file_get_contents("html/head.html");
+    echo file_get_contents("html/header.html");
+    echo file_get_contents("html/nav.html");
+    echo file_get_contents("html/promotion.html");
+    echo file_get_contents("html/end.html");
+}
+
+printIndexPage();
 /**
  * Created by PhpStorm.
- * User: Dmitr
+ * User: Dmitry Zaytsev
  * Date: 29.11.2018
  * Time: 14:19
  */
