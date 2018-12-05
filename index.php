7f133fe237455f72320da2d11cffb7e73768c858
Dmitry Zaytsev <dmitry.zaytsev83@gmail.com>
2018-12-05 13:51:12 +0300

    OOP style project and build footer

--- index.php
+++ index.php
@@ -1,13 +1,68 @@
 <?php
-function printIndexPage() {
-    echo file_get_contents("html/head.html");
-    echo file_get_contents("html/header.html");
-    echo file_get_contents("html/nav.html");
-    echo file_get_contents("html/promotion.html");
-    echo file_get_contents("html/end.html");
+
+class Head {
+    public static function printHead() {
+        return file_get_contents("html/head.html");
+    }
 }
 
-printIndexPage();
+class Header {
+    public static function printHeader() {
+        return file_get_contents("html/header.html");
+    }
+}
+
+class Nav {
+    public static function printNav() {
+        return file_get_contents("html/nav.html");
+    }
+}
+
+class Promotion {
+    public static function printPromotion() {
+        return file_get_contents("html/promotion.html");
+    }
+}
+
+class Footer {
+    public static function printFooter() {
+        return file_get_contents("html/footer.html");
+    }
+}
+
+class EndPage {
+    public static function printEndPage() {
+        return file_get_contents("html/end.html");
+    }
+}
+
+class MainPage {
+    private $head, $header, $nav, $footer, $end;
+
+    function __construct() {
+        $this->head = Head::printHead();
+        $this->header = Header::printHeader();
+        $this->nav = Nav::printNav();
+        $this->footer = Footer::printFooter();
+        $this->end = EndPage::printEndPage();
+    }
+
+    private function makeMainWrapper($content) {
+        return "<main class='main'>" . $content . "</main>";
+    }
+
+    public function render() {
+        echo $this->head;
+        echo $this->header;
+        echo $this->nav;
+        echo $this->makeMainWrapper('');
+        echo $this->footer;
+        echo $this->end;
+    }
+}
+
+$page = new MainPage();
+$page->render();
 /**
  * Created by PhpStorm.
  * User: Dmitry Zaytsev
